b23015a9c52ea94970d66d3395f8dcb298e26310
lordhollow <hollowscircus@gmail.com>
2016-05-29 00:56:24 +0900

    検索用のパネル

---
+++ ConEditor/GrepPanel.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace ConEditor
+{
+    partial class GrepPanel
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lstResult = new System.Windows.Forms.ListBox();
+            this.pControl = new System.Windows.Forms.Panel();
+            this.chkRegExp = new System.Windows.Forms.CheckBox();
+            this.chkCaseSensitive = new System.Windows.Forms.CheckBox();
+            this.chkFindAtEdit = new System.Windows.Forms.CheckBox();
+            this.chkIncremental = new System.Windows.Forms.CheckBox();
+            this.cmbKeyWord = new System.Windows.Forms.ComboBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.tmIncremental = new System.Windows.Forms.Timer(this.components);
+            this.pControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstResult
+            // 
+            this.lstResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstResult.FormattingEnabled = true;
+            this.lstResult.IntegralHeight = false;
+            this.lstResult.ItemHeight = 12;
+            this.lstResult.Location = new System.Drawing.Point(0, 50);
+            this.lstResult.Name = "lstResult";
+            this.lstResult.Size = new System.Drawing.Size(228, 152);
+            this.lstResult.TabIndex = 7;
+            this.lstResult.SelectedIndexChanged += new System.EventHandler(this.lstResult_SelectedIndexChanged);
+            // 
+            // pControl
+            // 
+            this.pControl.Controls.Add(this.chkRegExp);
+            this.pControl.Controls.Add(this.chkCaseSensitive);
+            this.pControl.Controls.Add(this.chkFindAtEdit);
+            this.pControl.Controls.Add(this.chkIncremental);
+            this.pControl.Controls.Add(this.cmbKeyWord);
+            this.pControl.Controls.Add(this.btnExecute);
+            this.pControl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pControl.Location = new System.Drawing.Point(0, 0);
+            this.pControl.Name = "pControl";
+            this.pControl.Size = new System.Drawing.Size(228, 50);
+            this.pControl.TabIndex = 8;
+            // 
+            // chkRegExp
+            // 
+            this.chkRegExp.AutoSize = true;
+            this.chkRegExp.Location = new System.Drawing.Point(47, 29);
+            this.chkRegExp.Name = "chkRegExp";
+            this.chkRegExp.Size = new System.Drawing.Size(63, 16);
+            this.chkRegExp.TabIndex = 4;
+            this.chkRegExp.Text = "RegExp";
+            this.chkRegExp.UseVisualStyleBackColor = true;
+            this.chkRegExp.CheckedChanged += new System.EventHandler(this.chkRegExp_CheckedChanged);
+            // 
+            // chkCaseSensitive
+            // 
+            this.chkCaseSensitive.AutoSize = true;
+            this.chkCaseSensitive.Location = new System.Drawing.Point(3, 29);
+            this.chkCaseSensitive.Name = "chkCaseSensitive";
+            this.chkCaseSensitive.Size = new System.Drawing.Size(38, 16);
+            this.chkCaseSensitive.TabIndex = 3;
+            this.chkCaseSensitive.Text = "Aa";
+            this.chkCaseSensitive.UseVisualStyleBackColor = true;
+            this.chkCaseSensitive.CheckedChanged += new System.EventHandler(this.chkCaseSensitive_CheckedChanged);
+            // 
+            // chkFindAtEdit
+            // 
+            this.chkFindAtEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkFindAtEdit.AutoSize = true;
+            this.chkFindAtEdit.Location = new System.Drawing.Point(177, 29);
+            this.chkFindAtEdit.Name = "chkFindAtEdit";
+            this.chkFindAtEdit.Size = new System.Drawing.Size(48, 16);
+            this.chkFindAtEdit.TabIndex = 7;
+            this.chkFindAtEdit.Text = "追従";
+            this.chkFindAtEdit.UseVisualStyleBackColor = true;
+            this.chkFindAtEdit.CheckedChanged += new System.EventHandler(this.chkFindAtEdit_CheckedChanged);
+            // 
+            // chkIncremental
+            // 
+            this.chkIncremental.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkIncremental.AutoSize = true;
+            this.chkIncremental.Location = new System.Drawing.Point(125, 29);
+            this.chkIncremental.Name = "chkIncremental";
+            this.chkIncremental.Size = new System.Drawing.Size(48, 16);
+            this.chkIncremental.TabIndex = 5;
+            this.chkIncremental.Text = "即時";
+            this.chkIncremental.UseVisualStyleBackColor = true;
+            this.chkIncremental.CheckedChanged += new System.EventHandler(this.chkIncremental_CheckedChanged);
+            // 
+            // cmbKeyWord
+            // 
+            this.cmbKeyWord.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbKeyWord.FormattingEnabled = true;
+            this.cmbKeyWord.Location = new System.Drawing.Point(3, 3);
+            this.cmbKeyWord.Name = "cmbKeyWord";
+            this.cmbKeyWord.Size = new System.Drawing.Size(165, 20);
+            this.cmbKeyWord.TabIndex = 2;
+            this.cmbKeyWord.TextChanged += new System.EventHandler(this.cmbKeyWord_TextChanged);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExecute.Location = new System.Drawing.Point(171, 0);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(57, 23);
+            this.btnExecute.TabIndex = 1;
+            this.btnExecute.Text = "検索";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblStatus.Location = new System.Drawing.Point(0, 202);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(50, 12);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "lblStatus";
+            // 
+            // tmIncremental
+            // 
+            this.tmIncremental.Interval = 500;
+            // 
+            // GrepPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lstResult);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pControl);
+            this.Name = "GrepPanel";
+            this.Size = new System.Drawing.Size(228, 214);
+            this.pControl.ResumeLayout(false);
+            this.pControl.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstResult;
+        private System.Windows.Forms.Panel pControl;
+        private System.Windows.Forms.CheckBox chkFindAtEdit;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.CheckBox chkIncremental;
+        private System.Windows.Forms.ComboBox cmbKeyWord;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.CheckBox chkRegExp;
+        private System.Windows.Forms.CheckBox chkCaseSensitive;
+        private System.Windows.Forms.Timer tmIncremental;
+
+    }
+}
---
+++ ConEditor/GrepPanel.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Sgry.Azuki;
+namespace ConEditor
+{
+    public partial class GrepPanel : UserControl
+    {
+        private const int incrementalIntervalMin = 10;
+        private const int resultWindow = 5;
+        private Binder binder;
+
+        List<SearchResult> results;
+        bool constructingResults = false;
+
+        static Regex ptnEscape = new Regex("\r|\n|\t");
+
+        bool condChangeFlag = false;
+
+        public GrepPanel(Binder binder)
+        {
+            InitializeComponent();
+            Status = "";
+            this.binder = binder;
+            binder.Document.ContentChanged += Document_ContentChanged;
+            Restore();
+
+        }
+
+        /// <summary>
+        /// 検索結果から何か選んだイベント
+        /// </summary>
+        public event EventHandler<GrepResultSelectedEventArgs> ResultSelectedChanged;
+
+        /// <summary>
+        /// 検索条件が変わったとき～
+        /// </summary>
+        public event EventHandler ConditionChanged;
+
+        /// <summary>
+        /// ステータス
+        /// </summary>
+        public string Status
+        {
+            get
+            {
+                return lblStatus.Text;
+            }
+            private set
+            {
+                lblStatus.Text = value;
+            }
+        }
+
+        /// <summary>
+        /// 検索条件
+        /// </summary>
+        public Regex Condition { get; private set; }
+
+        /// <summary>
+        /// インクリメンタルサーチの間隔(min=10ms)
+        /// </summary>
+        public int IncrementalInterval
+        {
+            get { return tmIncremental.Interval; }
+            set
+            {
+                if (value <= incrementalIntervalMin)
+                {
+                    value = incrementalIntervalMin;
+                }
+                tmIncremental.Interval = value;
+            }
+        }
+
+        /// <summary>
+        /// 最初の結果の場所へ
+        /// </summary>
+        public void SelectFirst()
+        {
+            if (lstResult.Items.Count != 0)
+            {
+                lstResult.SelectedIndex = 0;
+            }
+        }
+
+        /// <summary>
+        /// 最後の結果の場所へ
+        /// </summary>
+        public void SelectLast()
+        {
+            if (lstResult.Items.Count != 0)
+            {
+                lstResult.SelectedIndex = lstResult.Items.Count - 1;
+            }
+        }
+
+        /// <summary>
+        /// 次の結果の場所へ
+        /// </summary>
+        public void SelectNext()
+        {
+            if (lstResult.Items.Count != 0)
+            {
+                if (lstResult.SelectedIndex != lstResult.Items.Count - 1)
+                {
+                    lstResult.SelectedIndex++;
+                }
+                else
+                {
+                    lstResult.SelectedIndex = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 前の結果の場所へ
+        /// </summary>
+        public void SelectPrev()
+        {
+            if (lstResult.Items.Count != 0)
+            {
+                if (lstResult.SelectedIndex != 0)
+                {
+                    lstResult.SelectedIndex--;
+                }
+                else
+                {
+                    lstResult.SelectedIndex = lstResult.Items.Count - 1;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 履歴と設定のバックアップ
+        /// </summary>
+        public void Backup()
+        {
+        }
+
+        /// <summary>
+        /// 履歴と設定のレストア
+        /// </summary>
+        public void Restore()
+        {
+        }
+
+        /// <summary>
+        /// 変わった結果を通知
+        /// </summary>
+        private void onConditionChanged()
+        {
+            if (condChangeFlag == false) return;
+            ApplyConditions();
+            CreateResult();
+            condChangeFlag = false;
+
+            if (ConditionChanged != null)
+            {
+               ConditionChanged(this, EventArgs.Empty);
+            }
+        }
+
+        /// <summary>
+        /// 検索を実行する...
+        /// </summary>
+        private void ApplyConditions()
+        {
+            string ptn = cmbKeyWord.Text;
+            RegexOptions opt = RegexOptions.None;
+
+            if (!chkCaseSensitive.Checked) opt = RegexOptions.IgnoreCase;
+            if (!chkRegExp.Checked) ptn = Regex.Escape(ptn);
+
+            if (string.IsNullOrEmpty(ptn))
+            {
+                Condition = null;
+                Status = "";
+            }
+            else
+            {
+                try
+                {
+                    Condition = new Regex(ptn, opt);
+                }
+                catch
+                {
+                    Status = "正規表現パターン誤り";
+                }
+            }
+        }
+
+        /// <summary>
+        /// 検索結果の構築
+        /// </summary>
+        private void CreateResult()
+        {
+            constructingResults = true;
+
+            //検索結果を一回忘れる
+            lstResult.Items.Clear();
+            results = new List<SearchResult>();
+
+            //検索キーワードなければここで終わり
+            if (Condition == null) return;
+
+            //検索結果を一通り調べる
+            var result = binder.Document.FindNext(Condition, 0);
+            while (result != null)
+            {
+                //prevString
+                string prev = GetPrevString(result);
+
+                //trailingString
+                string trailing = GetTrailingString(result);
+
+                //検索結果
+                var matched = prev + "《" + escape(binder.Document.GetTextInRange(result.Begin, result.End)) + "》" + trailing;
+
+                results.Add(result);
+                lstResult.Items.Add(matched);
+
+                //次へ
+                result = binder.Document.FindNext(Condition, result.End);
+            }
+            constructingResults = false;
+        }
+
+        /// <summary>
+        /// 検索結果の前の文字列
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        private string GetPrevString(SearchResult result)
+        {
+            var begin = result.Begin - resultWindow;
+            if (begin < 0) begin = 0;
+            var beginHead = binder.Document.GetLineHeadIndexFromCharIndex(result.Begin);
+            if (beginHead > begin) begin = beginHead;
+            string prev = "";
+            if (begin != result.Begin)
+            {
+                prev = escape(binder.Document.GetTextInRange(begin, result.Begin));
+            }
+            return prev;
+        }
+
+        /// <summary>
+        /// 検索結果の後ろの文字列
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        private string GetTrailingString(SearchResult result)
+        {
+            var end = result.End + resultWindow;
+            if (end > binder.Document.Length) end = binder.Document.Length;
+            var endTail = binder.Document.GetLineEndIndexFromCharIndex(result.End);
+            if (endTail < end) end = endTail;
+            string trailing = "";
+            if (end != result.End)
+            {
+                trailing = escape(binder.Document.GetTextInRange(result.End, end));
+            }
+            return trailing;
+        }
+
+        /// <summary>
+        /// リストに入れるために加工する
+        /// </summary>
+        /// <param name="matched"></param>
+        /// <returns></returns>
+        private string escape(string matched)
+        {
+            return ptnEscape.Replace(matched, " ");
+        }
+
+        #region EventHandler
+
+        /// <summary>
+        /// フォーム閉じる時、登録イベントを解除
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GrepWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Backup();
+            binder.Document.ContentChanged -= Document_ContentChanged;
+        }
+
+        /// <summary>
+        /// 入力変えたとき、dirty
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmbKeyWord_TextChanged(object sender, EventArgs e)
+        {
+            condChangeFlag = true;
+        }
+
+        /// <summary>
+        /// Aa変えたとき、dirty
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void chkCaseSensitive_CheckedChanged(object sender, EventArgs e)
+        {
+            condChangeFlag = true;
+        }
+
+        /// <summary>
+        /// 正規表現設定切り替えたとき、dirty
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void chkRegExp_CheckedChanged(object sender, EventArgs e)
+        {
+            condChangeFlag = true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void chkIncremental_CheckedChanged(object sender, EventArgs e)
+        {
+            tmIncremental.Enabled = chkIncremental.Checked;
+        }
+
+        private void tmIncremental_Tick(object sender, EventArgs e)
+        {
+            onConditionChanged();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            condChangeFlag = true;
+            onConditionChanged();
+        }
+
+        private void chkFindAtEdit_CheckedChanged(object sender, EventArgs e)
+        {
+            //特に何もしない
+        }
+
+        private void lstResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (constructingResults) return;
+            if (results == null) return;
+            var d = lstResult.SelectedIndex;
+            if ((d >= 0) && (d < results.Count))
+            {
+                var result = results[d];
+
+                if (ResultSelectedChanged != null)
+                {
+                    ResultSelectedChanged(this, new GrepResultSelectedEventArgs(result));
+                }
+            }
+        }
+
+        void Document_ContentChanged(object sender, ContentChangedEventArgs e)
+        {
+            if (chkFindAtEdit.Checked && Condition != null)
+            {
+                CreateResult();
+            }
+        }
+
+        #endregion
+    }
+}
